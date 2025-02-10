00a78bf46a70fab6ab80e367b59ebe202b834007
donnie4w <donnie4w@gmail.com>
2025-02-10 11:03:55 +0800

    Update const.go

--- const.go
+++ const.go
@@ -8,11 +8,38 @@ package tssgo
 type TIMTYPE byte
 
 const (
-	ADMPING TIMTYPE = 11
-	ADMAUTH TIMTYPE = 12
-	ADMSUB  TIMTYPE = 13
+	ADMPING         TIMTYPE = 11
+	ADMAUTH         TIMTYPE = 12
+	ADMSUB          TIMTYPE = 13
+	BIGBINARYSTREAM TIMTYPE = 17
+)
+
+const (
+	SOURCE_OS   int8 = 1
+	SOURCE_USER int8 = 2
+	SOURCE_ROOM int8 = 3
+)
+
+var (
+	ONLINE   int8 = 1
+	OFFLIINE int8 = 2
+)
+
+const (
+	ORDER_INOF      int8 = 1
+	ORDER_REVOKE    int8 = 2
+	ORDER_BURN      int8 = 3
+	ORDER_BUSINESS  int8 = 4
+	ORDER_STREAM    int8 = 5
+	ORDER_BIGSTRING int8 = 6
+	ORDER_BIGBINARY int8 = 7
+	ORDER_RESERVED  int8 = 30
 )
 
 var (
 	SUB_ONLINE int8 = 1
+)
+
+var (
+	SEP_BIN = byte(131)
 )
